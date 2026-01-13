ada128075315fb6fd40162cddb76a2622376addf
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2026-01-13 21:31:16 +0600

    Refactor ToDo component to include delete functionality and update App component for data fetching

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import ToDo from './components/ToDo'
 
 function App() {
   const [todo, setTodo] = useState("")
-  const [datas , setDatas] = useState([])
-  const none = todo
+  const [datas, setDatas] = useState([])
 
-
-  useEffect(()=>{
+  const todoApp = () => {
     fetch("http://localhost:3000/api/get")
-    .then(res => res.json())
-    .then(data => setDatas(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setDatas(data))
+  }
+
+  useEffect(() => {
+    todoApp()
+  }, [])
 
   const handleSend = (e) => {
     e.preventDefault()
@@ -26,13 +28,10 @@ function App() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ toDo: todo })
       })
+      todoApp()
     }
-    
+
   }
-  
-  console.log(datas);
-
-
 
   return (
     <>
@@ -41,15 +40,18 @@ function App() {
           <h1 className='py-5 text-[40px] font-bold font-Delius'>ToDo App</h1>
           <form action="">
             <input value={none} onChange={(e) => setTodo(e.target.value)} placeholder='Enter your task' type="text" className='border-2 px-5 py-3 rounded-[7px] font-bold' />
-            <button onClick={handleSend} className='ml-5 p-3 border-2 bg-amber-500 text-black font-bold cursor-pointer'>Add</button>
+            <button onDle onClick={handleSend} className='ml-5 p-3 border-2 bg-amber-500 text-black font-bold cursor-pointer'>Add</button>
           </form>
         </div>
-       {datas.map((item)=>(
-        <ToDo taskName={item.toDo}/>
-       ))}
+        {datas.map((item) => (
+          <ToDo key={item._id} id={item._id} taskName={item.toDo} />
+        ))}
       </Container>
     </>
   )
 }
 
 export default App
+
+
+
--- client/src/components/ToDo.jsx
+++ client/src/components/ToDo.jsx
@@ -3,14 +3,19 @@ import { AiFillEdit } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
 
 
-const ToDo = ({ taskName }) => {
+const ToDo = ({id, taskName }) => {
+    const handleDlt = async (id)=>{
+        await fetch(`http://localhost:3000/api/delete/${id}`,{
+            method : "DELETE"
+        }) 
+    }
     return (
         <>
             <div className="my-[10px] border-2 px-5 py-3 rounded-[7px] font-bold w-[400px] m-auto flex justify-between items-center">
                 <h1>{taskName}</h1>
                 <div className="flex text-[25px] gap-x-5">
-                    <button className='cursor-pointer'><AiFillEdit /></button>
-                    <button className='cursor-pointer'><MdDelete /></button>
+                    <button  className='cursor-pointer'><AiFillEdit /></button>
+                    <button  onClick={()=> handleDlt(id)} className='cursor-pointer'><MdDelete /></button>
                 </div>
             </div>
         </>
