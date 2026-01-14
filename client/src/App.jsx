54d548d0c8c9a7e8e57cb90d2f4e0fe4bce175e8
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2026-01-14 11:42:59 +0600

    Add the auto rendered when a task has added without refreshing the whole page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import ToDo from './components/ToDo'
 function App() {
   const [todo, setTodo] = useState("")
   const [datas, setDatas] = useState([])
+  const [updateUI , setUpdateUI] = useState(false)
 
 
 
@@ -13,7 +14,7 @@ function App() {
     fetch("http://localhost:3000/api/get")
       .then(res => res.json())
       .then(data => setDatas(data))
-  }, [])
+  }, [updateUI])
 
   const handleSend = (e) => {
     e.preventDefault()
@@ -26,7 +27,8 @@ function App() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ toDo: todo })
       })
-      window.location.reload()
+      .then(()=> setTodo(""))
+      .then(()=> setUpdateUI((prevState)=> !prevState))
     }
 
   }
@@ -37,7 +39,7 @@ function App() {
         <div className="text-center mb-[20px]">
           <h1 className='py-5 text-[40px] font-bold font-Delius'>ToDo App</h1>
           <form action="">
-            <input onChange={(e) => setTodo(e.target.value)} placeholder='Enter your task' type="text" className='border-2 px-5 py-3 rounded-[7px] font-bold' />
+            <input value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Enter your task' type="text" className='border-2 px-5 py-3 rounded-[7px] font-bold' />
             <button onClick={handleSend} className='ml-5 p-3 border-2 bg-amber-500 text-black font-bold cursor-pointer'>Add</button>
           </form>
         </div>
